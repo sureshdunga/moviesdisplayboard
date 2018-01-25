c833b05605a0aa8914cc5979bce74ceb1ed599e0
sureshkumar.dunga <sureshkumar.dunga@zohocorp.com>
2018-01-25 09:41:12 +0530

    add genre and trailer to movies detail

--- app/components/movies/m-detail.js
+++ app/components/movies/m-detail.js
@@ -1,6 +1,26 @@
-import Component from '@ember/component';
+import Component from "@ember/component";
+import { computed } from "@ember/object";
 
 export default Component.extend({
-  attributeBindings:["_layout:layout"],
-  _layout:"column center-center"
+  attributeBindings: ["_layout:layout"],
+  _layout: "column center-center",
+
+  trailerObjects: computed("movie.videos.results", function() {
+    let videoResults = this.get("movie.videos.results");
+    if (!videoResults.length) {
+      return null;
+    }
+
+    return videoResults.filter(t => t.type === "Trailer").map(t => {
+      switch (t.site) {
+        //TODO: Should find all possible sites, and a fallback state
+        case "YouTube":
+          t.url = `https://www.youtube.com/watch?v=${t.key}`;
+          break;
+        default:
+          t.url = `https://www.youtube.com/watch?v=${t.key}`;
+      }
+      return t;
+    });
+  })
 });
